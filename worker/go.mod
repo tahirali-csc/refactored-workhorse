9e64313eb7f2a3c719e95d76188002b0dd866e10
tahir <tahir@tahirs-MacBook-Pro.local>
2020-10-18 11:37:03 -0400

    Adding Git testing code

--- worker/go.mod
+++ worker/go.mod
@@ -22,12 +22,14 @@ require (
 	github.com/docker/go-events v0.0.0-20190806004212-e31b211e4f1c // indirect
 	github.com/docker/go-metrics v0.0.1 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
+	github.com/go-git/go-git/v5 v5.1.0
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/sirupsen/logrus v1.6.0 // indirect
-	google.golang.org/grpc v1.31.1 // indirect
 	github.com/r3labs/sse v0.0.0-20200819143619-1491ab50668f
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073
+	google.golang.org/grpc v1.31.1 // indirect
 )
--- worker/main.go
+++ worker/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	ssh2 "golang.org/x/crypto/ssh"
+	"log"
+
 	"github.com/workhorse/worker/pkg/executor"
 	"github.com/workhorse/worker/pkg/executor/buildstep"
 	"net/http"
+	"os"
 )
 
-func main() {
+func _main() {
 
 	buildOrchestrator := executor.NewBuildOrchestrator()
 	go buildOrchestrator.Start()
@@ -18,4 +24,34 @@ func main() {
 
 	http.ListenAndServe("localhost:8086", nil)
 
+}
+
+func main(){
+	err := gitClone("/Users/tahir/workspace/git-tutorial")
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func gitClone(dir string) error{
+
+	publicKeys, err := ssh.NewPublicKeysFromFile("git", "/Users/tahir/.ssh/id_rsa", "")
+	publicKeys.HostKeyCallback = ssh2.InsecureIgnoreHostKey()
+
+	if err != nil {
+		return err
+	}
+
+	_, err = git.PlainClone(dir, false, &git.CloneOptions{
+		URL: "git@github.com:tahirali-csc/hello-app.git",
+		Progress: os.Stdout,
+		Auth:     publicKeys,
+
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
